a44e80b12a46216ac06fb0238097e6a1b1e8f425
tareqmeg <tareq.megdadi@gmail.com>
2021-04-26 13:51:27 +0300

    lab-06

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const server = express();
 
-
 const PORT = process.env.PORT || 3000;
+server.use(cors());
 
 server.get('/',(req,res)=>{
   res.status(200).send('Hi from the data page, I am the server !!!');
@@ -16,6 +17,19 @@ server.get('/location',(req,res) =>{
   let locationRes = new Location(locationData);
 
   res.send(locationRes);
+
+});
+
+server.get('/weather',(req,res) =>{
+  let weathData = require('./data/weather.json');
+  let newWarr =[];
+  weathData.data.forEach((item)=>{
+    let wearherData = new Weather(item);
+    newWarr.push(wearherData);
+  });
+
+
+  res.send(newWarr);
 });
 
 function Location(locData){
@@ -23,6 +37,13 @@ function Location(locData){
   this.formatted_query = locData[0].display_name;
   this.latitude = locData[0].lat;
   this.longitude = locData[0].lon;
+}
+function Weather(weathData){
+  this.forcast = weathData.weather.description;
+  this.time = weathData.valid_data;
+
+
+
 }
 
 
